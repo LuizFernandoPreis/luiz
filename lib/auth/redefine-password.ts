962257ad95672471cc82b5b4a14157619c8ea331
kleinerschein <luizfernandopreis4@gmail.com>
2024-09-17 21:21:56 -0300

    .

--- lib/auth/redefine-password.ts
+++ lib/auth/redefine-password.ts
@@ -2,9 +2,9 @@
 
 import { action } from '@/actions'
 import jwt from 'jsonwebtoken'
-import { KEY as JWT_SECRET } from '../constants'
-import { confirmPasswordReset } from '../email/transactional-mails'
 import { resetPasswordRoute } from '../routes'
+
+const JWT_SECRET = "JWT-CONFIG"
 
 export const redefinePassword = async ({ email }: { email: string }) => {
   if (!(await action.usuario().find({ where: { email } }))) return
@@ -13,5 +13,4 @@ export const redefinePassword = async ({ email }: { email: string }) => {
 
   const url = `${process.env.NEXT_PUBLIC_URL}${resetPasswordRoute}${token}`
 
-  await confirmPasswordReset({ email, url })
 }
