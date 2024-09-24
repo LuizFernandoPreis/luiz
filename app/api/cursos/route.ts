c96411c82e7122de6d8702ea48ab3b0eb41797ec
LuizFernandoPreis <149117955+LuizFernandoPreis@users.noreply.github.com>
2024-09-24 00:26:21 -0300

    ajuste cors

--- app/api/cursos/route.ts
+++ app/api/cursos/route.ts
@@ -12,10 +12,17 @@ export async function GET(request: NextRequest) {
         'Authorization': 'Basic UU9JRjlQR0ZJZ2M4cW5NOG04Tmc2VU1ET2s5Z21lUXNpRkpPS2p5WjpXamI0MktHNnplSFpJVWZLMHhNUVR5QXFPY2JNUEFXY3Z1OUhyZnUzRm15MGIzcUdoWVg4RTVqS2cwcHRXbTZya2RsbGIwbUpqUDQ2VGtQNGpMVnpDenBWZ29jM1BTam1SbVdOb2M4akJacTlLdEk1NUtYOXE1ajZ2bVpzUWl0Mg=='
       }
     });
+    
     const data = await response.json();
-    return NextResponse.json(data.results);
-  } catch (error) {
-    return NextResponse.json(error);
-  }
 
+    // Retorna a resposta com cabeçalhos CORS
+    const res = NextResponse.json(data.results);
+    res.headers.set('Access-Control-Allow-Origin', 'https://tcc-ifsc.vercel.app'); // Substitua pelo domínio que você deseja permitir
+    res.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS'); // Métodos permitidos
+    res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Cabeçalhos permitidos
+
+    return res;
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch data' });
+  }
 }
