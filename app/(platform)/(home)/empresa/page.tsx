c43d1825176333480facefd6f60c006f175cb371
kleinerschein <luizfernandopreis4@gmail.com>
2024-10-08 20:09:40 -0300

    ajuste sou empresa

--- app/(platform)/(home)/empresa/page.tsx
+++ app/(platform)/(home)/empresa/page.tsx
@@ -5,7 +5,7 @@ export default async function Empresa() {
   return (
     <div>
       <HeroSection />
-      <div className="flex min-h-40 bg-mercury justify-center gap-4 flex-col md:flex-row">
+      <div className="flex min-h-40 bg-mercury justify-center gap-4 flex-col md:flex-row grow">
         <div className="flex flex-col md:w-60 mt-4">
           <div className="flex">
             <BookCheck />
@@ -45,27 +45,28 @@ export default async function Empresa() {
       <div className="flex justify-center mt-8 bg-mercury p-8 mb-16">
         <div className="flex flex-col justify-center w-full">
           <div>
-            <h1 className="font-bold text-3xl text-center mt-2">
+            <h1 className="font-bold text-3xl text-center mt-2 z-20">
               Como Funciona?
             </h1>
-            <div className="flex flex-col gap-4 mt-8">
-              <div className="shadow-md ml-[600px] rounded-lg p-4 w-fit bg-white">
+            <div className="flex flex-col gap-4 mt-8 z-20">
+              <div className="shadow-md ml-[600px] rounded-lg p-4 w-fit bg-white z-20">
                 <h1 className="font-bold">1° Passo:</h1>
                 <p>Cadastrar a oportunidade no site</p>
               </div>
-              <div className="flex justify-end">
-                <div className="shadow-md rounded-lg mr-[600px] p-4 w-fit bg-white">
+              <div className="flex justify-center z-20">
+                <div className="shadow-md rounded-lg p-4 w-fit bg-white">
                   <h1 className="font-bold">2° Passo:</h1>
                   <p>Vincular cursos com os conhecimentos requeridos</p>
                 </div>
               </div>
-              <div>
-                <div className="shadow-md rounded-lg ml-[600px] p-4 w-fit bg-white">
+              <div className="flex justify-center z-20">
+                <div className="shadow-md rounded-lg p-4 w-fit bg-white">
                   <h1 className="font-bold">3° Passo:</h1>
                   <p>Esperar para que candidatos qualificados se inscrevam</p>
                 </div>
               </div>
             </div>
+            <div className="relative bottom-[85%] left-1/2 h-[90%] bg-black/50 w-0.5 z-10"></div>
           </div>
         </div>
       </div>
