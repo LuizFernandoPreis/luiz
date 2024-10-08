bf146eda24a5571c2655789622b0260b00be485e
LuizFernandoPreis <149117955+LuizFernandoPreis@users.noreply.github.com>
2024-10-08 00:44:07 -0300

    mais delete

--- actions/index.ts
+++ actions/index.ts
@@ -1,13 +1,5 @@
 import { usuarioAction } from './usuario'
-import { ufAction } from './uf'
-import { cidadeAction } from './cidade'
-import { enderecoAction } from './endereco'
-import { clienteAction } from './cliente'
 
 export const action = {
     usuario: usuarioAction,
-    uf: ufAction,
-    cidade: cidadeAction,
-    endereco: enderecoAction,
-    cliente: clienteAction,
 }
