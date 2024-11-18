507fb77a639e21323841ee930bc42c2a7a75d215
kleinerschein <luizfernandopreis4@gmail.com>
2024-11-18 13:10:01 -0300

    ajuste no perfilHero

--- app/(platform)/(home)/perfil/_components/perfilHero.tsx
+++ app/(platform)/(home)/perfil/_components/perfilHero.tsx
@@ -1,39 +1,44 @@
 "use client";
+
 import { Usuario } from "@prisma/client";
 import { useState } from "react";
+import Image from "next/image";
 import ModalUpdateCapaImage from "./modalUpdateCapa";
-type heroProps = {
+
+type HeroProps = {
   children: React.ReactNode;
   user: Usuario;
 };
-export default function HeroSection({ children, user }: heroProps) {
+
+export default function HeroSection({ children, user }: HeroProps) {
   const [isModalCapaOpen, setIsModalCapaOpen] = useState(false);
+
+  const backgroundImage = user.userCapaImage || "/icons/capaPlaceholder.png";
+
   return (
-    <div
-      className="relative isolate overflow-hidden bg-gray-900 sm:py-32 min-h-[300px]"
+    <div className="relative isolate overflow-hidden bg-gray-900 sm:py-32 min-h-[300px]">
+      {/* Imagem de fundo */}
+      <Image
+        src={backgroundImage}
+        alt="Capa do usuário"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        className="absolute inset-0 -z-20 opacity-50"
+        onClick={() => setIsModalCapaOpen(true)}
+        priority // Carrega a imagem com prioridade
+      />
       
-    >
-      <div className="absolute inset-0 -z-10 opacity-50" onClick={() => setIsModalCapaOpen(true)}></div>
-      <div
-        className="absolute inset-0 -z-20 bg-cover bg-center"
-        style={{
-          backgroundImage: `${
-            user.userCapaImage
-              ? `url(${user.userCapaImage})`
-              : `url(/icons/capaPlaceholder.png)`
-          }`,
-          backgroundAttachment: "fixed",
-        }}
-      >
-       
-      </div> 
+      {/* Modal para alterar a capa */}
       {isModalCapaOpen && (
-          <ModalUpdateCapaImage
-            userId={user.id}
-            isOpen={isModalCapaOpen}
-            onClose={() => setIsModalCapaOpen(false)}
-          />
-        )}
+        <ModalUpdateCapaImage
+          userId={user.id}
+          isOpen={isModalCapaOpen}
+          onClose={() => setIsModalCapaOpen(false)}
+        />
+      )}
+
+      {/* Conteúdo adicional */}
       {children}
     </div>
   );
